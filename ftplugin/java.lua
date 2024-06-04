4e5647374cf649c89a1ad1080d3cebbdec7288a7
Ravenelle <lrav35@gmail.com>
2024-06-04 10:32:11 -0400

    getting closer - nvim still cant find jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,9 +1,3 @@
--- local config = {
--- 	cmd = { "~/plugins/org.eclipse.jdt.ls.core_1.36.0.202405301306.jar" },
--- 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
--- }
--- require("jdtls").start_or_attach(config)
-
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
 local workspace_dir = '~/code/liberty/' .. project_name
@@ -16,7 +10,7 @@ local config = {
 
     -- 💀
     '/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home',
-            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
     '-Dosgi.bundles.defaultStartLevel=4',
@@ -25,26 +19,29 @@ local config = {
     '-Dlog.level=ALL',
     '-Xmx1g',
     '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
 
     -- 💀
-    '-jar', '~/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.2.1000.v20240507-1834.jar',
-         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
-         -- Must point to the                                                     Change this to
-         -- eclipse.jdt.ls installation                                           the actual version
-
+    '-jar',
+    '~/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.2.1000.v20240507-1834.jar',
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+    -- Must point to the                                                     Change this to
+    -- eclipse.jdt.ls installation                                           the actual version
 
     -- 💀
-    '-configuration', '~/plugins/config_mac',
-                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
-                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
-                    -- eclipse.jdt.ls installation            Depending on your system.
-
+    '-configuration',
+    '~/plugins/config_mac',
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
 
     -- 💀
     -- See `data directory configuration` section in the README
-    '-data', workspace_dir,
+    '-data',
+    workspace_dir,
   },
 
   -- 💀
@@ -53,14 +50,13 @@ local config = {
   --
   -- vim.fs.root requires Neovim 0.10.
   -- If you're using an earlier version, use: require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-  root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"})
+  root_dir = vim.fs.root(0, { '.git', 'mvnw', 'gradlew' }),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   -- for a list of options
   settings = {
-    java = {
-    }
+    java = {},
   },
 
   -- Language server `initializationOptions`
@@ -71,7 +67,7 @@ local config = {
   --
   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
   init_options = {
-    bundles = {}
+    bundles = {},
   },
 }
 -- This starts a new client & server,
